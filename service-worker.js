7b3c5020fcb9543fa2bba134d1cc2064dc0da9d9
NihongoTopics <44304801+NihongoTopics@users.noreply.github.com>
2025-05-30 13:20:45 +0900

    web app

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "tokyo-temp-v1";
+const urlsToCache = [
+  "index.html",
+  "help.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
